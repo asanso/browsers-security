17a72513c8ad28d951830578dcf72c398cf74b37
Antonio Sanso <antonio.sanso@gmail.com>
2015-11-26 09:14:47 +0100

    Create helloWorldJsonvar express = require("express");.js

---
+++ nosniff/helloWorldJsonvar express = require("express");.js
@@ -0,0 +1,16 @@
+var express = require("express");
+
+var app = express();
+
+var resource = {
+	"name": "Protected Resource"
+};
+
+app.get("/oauth/authorize", function(req, res){
+  res.json(resource);
+}
+
+var server = app.listen(9002, 'localhost', function () {
+  var host = server.address().address;
+  var port = server.address().port;
+});
