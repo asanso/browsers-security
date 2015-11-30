764d4c471563af66575c89d560af0df20503a887
Antonio Sanso <asanso@adobe.com>
2015-11-30 13:41:05 +0100

    add no sniff

---
+++ nosniff/noSniff.js
@@ -0,0 +1,15 @@
+var express = require("express");
+
+var app = express();
+
+/*app.use(function (req, res, next) {	 
+	res.setHeader('X-Content-Type-Options', 'no-sniff');
+	next();
+});*/
+
+app.use('/', express.static('files'));
+
+var server = app.listen(9002, 'localhost', function () {
+  var host = server.address().address;
+  var port = server.address().port;
+});
