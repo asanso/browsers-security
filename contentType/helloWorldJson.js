acfa1339d2d9d4d1af4c9a9fb2ed425ecea0edc6
Antonio Sanso <asanso@adobe.com>
2015-12-04 17:59:01 +0100

    update

--- contentType/helloWorldJson.js
+++ contentType/helloWorldJson.js
@@ -1,4 +1,6 @@
 var express = require("express");
+var nosql = require('nosql').load('database.nosql');
+
 var app = express();
 
 var resource = {
@@ -8,12 +10,28 @@ var resource = {
 app.get("/", function(req, res){
 
   if (req.query.name) {	
-  	 res.send('{"helloWorld": "'+req.query.name+'"}');
+    nosql.insert(req.query.name);
+      res.writeHead(302,
+     {Location: '/'}
+    );
+    res.end();
   } else if (req.query.surname) {
   	 resource.helloWorld = req.query.surname;
 	   res.json(resource);
   } else if (req.query.title) {
   	 res.send('{"helloWorld": "'+req.query.title+'"}');
+  } else {
+    nosql.one(function(name) {
+        return name; ;
+      },function(err, name) {
+    if (name) {      
+      nosql.clear();
+      res.send('{"helloWorld": "'+name+'"}');
+      return;
+    } else {       
+      res.send('{"helloWorld"}');
+      return;
+    }});
   }
 });
 
