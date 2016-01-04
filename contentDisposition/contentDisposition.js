ce6b471f233ecd048ffa13ff80919908fef306bd
Antonio Sanso <antonio.sanso@gmail.com>
2016-01-04 11:01:07 +0100

    Update contentDisposition.js

--- contentDisposition/contentDisposition.js
+++ contentDisposition/contentDisposition.js
@@ -1,1 +1,15 @@
+var express = require("express");
 
+var app = express();
+
+app.use(function (req, res, next) {	 
+	res.setHeader('Content-Disposition', 'attachement');
+	next();
+});
+
+app.use('/', express.static('files'));
+
+var server = app.listen(9002, 'localhost', function () {
+  var host = server.address().address;
+  var port = server.address().port;
+});
